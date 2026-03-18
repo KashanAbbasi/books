534a7917fe9d5c5ec19142c5e5512226738cd47a
KashanAbbasi <spam40211@gmail.com>
2026-03-18 20:30:04 +0500

    Update Supabase configuration with new URL and key

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = window.APP_CONFIG || {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://lwisrgraxwvbjnbdowwy.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3aXNyZ3JheHd2YmpuYmRvd3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NDI1MTYsImV4cCI6MjA4OTQxODUxNn0.MvVlSEjg9m94_GiJVCY-WYCMBProaGzzZDeKN4vRPpk'
 };
